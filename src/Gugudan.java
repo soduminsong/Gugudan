d6755c1b0416e40593399cbdf67e08c285ac17fe
alstj <alstj@LAPTOP-C4PUI2GG>
2018-11-14 00:12:40 +0900

    자바 기초 (feat. 구구단)

---
+++ src/Gugudan.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class Gugudan {
+	public static void main(String[] args) {
+		// 2단
+//		System.out.println("2단");
+//		System.out.println(2*1);
+//		System.out.println(2*2);
+//		System.out.println(2*3);
+//		System.out.println(2*4);
+//		System.out.println(2*5);
+//		System.out.println(2*6);
+//		System.out.println(2*7);
+//		System.out.println(2*8);
+//		System.out.println(2*9);
+		
+		// 3단
+//		System.out.println("3단");
+//		System.out.println(3*1);
+//		System.out.println(3*2);
+//		System.out.println(3*3);
+//		System.out.println(3*4);
+//		System.out.println(3*5);
+//		System.out.println(3*6);
+//		System.out.println(3*7);
+//		System.out.println(3*8);
+//		System.out.println(3*9);
+		
+		// 4단
+//		System.out.println("4단");
+//		int result = 4 * 1;
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+//		result = 4 * 3;
+//		System.out.println(result);
+//		result = 4 * 4;
+//		System.out.println(result);
+//		result = 4 * 5;
+//		System.out.println(result);
+//		result = 4 * 6;
+//		System.out.println(result);
+//		result = 4 * 7;
+//		System.out.println(result);
+//		result = 4 * 8;
+//		System.out.println(result);
+//		result = 4 * 9;
+//		System.out.println(result);
+		
+		//5단
+//		System.out.println("구구단 중 출력할 단은 ? ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		System.out.println(number * 1);
+//		System.out.println(number * 2);
+//		System.out.println(number * 3);
+//		System.out.println(number * 4);
+//		System.out.println(number * 5);
+//		System.out.println(number * 6);
+//		System.out.println(number * 7);
+//		System.out.println(number * 8);
+//		System.out.println(number * 9);
+		
+//		int i = 1;
+//		while (i < 10) {
+//			System.out.println(6 * i);
+//			i++;
+//		}
+//		for(int j = 1; j < 10; j++) {
+//			System.out.println(7 * j);
+//		}
+		
+//		System.out.println("구구단 입력 : ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		System.out.println("number : " + number);
+//		
+//		if (number < 2) {
+//			System.out.println("2이상의 수를 입력 하시오");
+//		}else if (number > 9) {
+//			System.out.println("9이하의 수를 입력 하시오 ");
+//		}else {
+//			for(int i =0;i<10;i++) {
+//				System.out.println(i * number);
+//			}
+//		}
+		int[] result = new int[9];
+		for(int i =0;i<result.length; i++) {
+			result[i] = 2 * (i + 1);
+		}
+		
+		for(int i=0;i<result.length;i++) {
+			System.out.println(result[i]);
+		}
+		int[] times3 = new int[9];
+		for(int i =0;i<times3.length; i++) {
+			times3[i] = 3 * (i + 1);
+		}
+		
+		for(int i=0;i<times3.length;i++) {
+			System.out.println(times3[i]);
+		}
+	}
+}
